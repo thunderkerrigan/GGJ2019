58ab5660cd24570cb05eb12ded0f623f1b73811a
Briqn <pitet.brian@gmail.com>
2019-01-27 13:36:10 +0100

    Scene Management
    Optimisation of assets

---
+++ Assets/Scripts/LoadMainScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadMainScene : MonoBehaviour
+{
+    public void ManageScene(string scenename)
+        {
+            SceneManager.LoadScene(scenename, LoadSceneMode.Single);
+        }
+
+}
---
+++ Assets/Scripts/QuitOnClick.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class QuitOnClick : MonoBehaviour
+{
+
+    public void Quit()
+    {
+#if (UNITY_EDITOR)
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+
+        }
+#else
+		{
+			Application.Quit ();
+
+		}
+#endif
+    }
+
+}
